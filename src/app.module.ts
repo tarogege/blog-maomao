7490987beaee6ff2e6389f84f98766861afb6268
maoyuting <maoyuting@huayun.com>
2022-03-15 19:45:46 +0800

    fix: cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
+
 const localDbUrl =
   'mongodb://admin:123456@localhost:27017/nest?authSource=admin';
 
