3c0c343d6666d3178cedf3b9824e110e87833571
maoyuting <maoyuting@huayun.com>
2022-03-21 15:49:04 +0800

    fix: branch fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { ArticleModule } from './article/article.module';
-
 
 const localDbUrl =
   'mongodb://admin:123456@localhost:27017/nest?authSource=admin';
@@ -15,7 +13,7 @@ const localDbUrl =
   let mongodbUrl = uri || localDbUrl
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(mongodbUrl), AuthModule, ArticleModule],
+  imports: [UsersModule, MongooseModule.forRoot(mongodbUrl), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
