017419bc69e4d4435d807814cbdc534bb9d04624
maoyuting <maoyuting@huayun.com>
2022-03-21 09:18:01 +0800

    fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,40 +35,19 @@ export class UsersService {
     // get current
     async getUser(token: string) {
         console.log(token, 'token')
-<<<<<<< HEAD
         const userInfo = await this.authService.verify(token)
         const userOne = await this.userModel.findById(userInfo?.userId)
         if (!userOne) {
             throw new NotFoundError('用户不存在')
-=======
-        if (token) {
-            const userInfo = await this.authService.verify(token.slice(7))
-            const userOne = await this.userModel.findById(userInfo?.userId)
-            if (!userOne) {
-                throw new NotFoundError('用户不存在')
-            }
-            return { user: userOne }
->>>>>>> 89a82b4266b4eb0ae894969edfa230186d7f8b9a
         }
         throw new NotFoundError('无权限')
     }
 
     async update(user: User, token: string) {
-<<<<<<< HEAD
         const userInfo = await this.authService.verify(token)
         const userOne = this.userModel.findById(userInfo?.userId)
         if (!userOne) {
             throw new NotFoundError('用户不存在')
-=======
-        if (token) {
-            const userInfo = await this.authService.verify(token.slice(7))
-            const userOne = this.userModel.findById(userInfo?.userId)
-            if (!userOne) {
-                throw new NotFoundError('用户不存在')
-            }
-            await this.userModel.updateOne({_id: userInfo?.userId}, {$set: user})
-            return '更新成功'
->>>>>>> 89a82b4266b4eb0ae894969edfa230186d7f8b9a
         }
         throw new NotFoundError('无权限')
     }
