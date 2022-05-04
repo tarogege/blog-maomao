536a8a8c6237e12a8b0f5a6cc2ca63f69ca1ee0e
maoyuting@huayun.com <maoyuting@huayun.com>
2022-05-04 21:21:39 +0800

    取消get articles参数法

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -13,7 +13,12 @@ export class ArticleController {
     getArticles(@Query('tag') tag: string, @Query('author') author: string, @Query('name') name: string, @Query('limit') limit: number, @Query('page') page: number ) {
         // @param @query @body
         // 中文可能会乱码，所以要用encodeURIComponet，decodeURIComponent
-        return this.articleService.getArticles({tag, author, limit, page, name})
+        return this.articleService.getArticles({
+            tag, 
+            author, 
+            // limit, 
+            // page, 
+            name})
     }
 
     @Get(':id')
